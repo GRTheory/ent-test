4a8fc8b3eed687377277292dddec867421b9da42
zhangsan33 <8546260+zhangsan3356@user.noreply.gitee.com>
2023-02-06 14:06:25 +0800

    add edge example

--- start.go
+++ start.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/GRTheory/ent-test/ent"
+	"github.com/GRTheory/ent-test/ent/car"
 	"github.com/GRTheory/ent-test/ent/user"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -23,10 +25,15 @@ func main() {
 
 	// user, err := createUser(context.Background(), client)
 	user, err := QueryUser(context.Background(), client)
+	// user, err  := CreateCars(context.Background(), client)
 	if err != nil {
 		log.Fatalf("failed creating a new user table: %v", err)
 	}
 	log.Println("user", user)
+	err = QueryCars(context.Background(), user)
+	if err != nil {
+		log.Fatalf("failed querying the cars owned by the specific user: %v", err)
+	}
 }
 
 func createUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
@@ -52,4 +59,58 @@ func QueryUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
 	}
 	log.Println("user returned: ", u)
 	return u, nil
+}
+
+func CreateCars(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	// Create a new car with model "Tesla".
+	tesla, err := client.Car.
+		Create().SetModel("Tesla").
+		SetRegisteredAt(time.Now()).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating car: %v", err)
+	}
+	log.Println("car was created: ", tesla)
+
+	// Create a new car with model "Ford".
+	ford, err := client.Car.
+		Create().
+		SetModel("Ford").
+		SetRegisteredAt(time.Now()).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating car: %v", err)
+	}
+	log.Println("car was created: ", ford)
+
+	// Create a new user, and add it the 2 cars.
+	a8m, err := client.User.
+		Create().
+		SetAge(30).
+		SetName("a8m").
+		AddCars(tesla, ford).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %v", err)
+	}
+	log.Println("user was created: ", a8m)
+	return a8m, nil
+}
+
+func QueryCars(ctx context.Context, a8m *ent.User) error {
+	cars, err := a8m.QueryCars().All(ctx)
+	if err != nil {
+		return fmt.Errorf("failed querying user cars: %w", err)
+	}
+	log.Println("returned cars:", cars)
+
+	// What about filtering specific cars.
+	ford, err := a8m.QueryCars().
+		Where(car.Model("Ford")).
+		Only(ctx)
+	if err != nil {
+		return fmt.Errorf("failed querying user cars: %w", err)
+	}
+	log.Println(ford)
+	return nil
 }
